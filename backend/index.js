8d2cc58f555357e069cc2d6ffdde7e4ed77dcc71
hollyjohanna <hollyjohanna.robbins@gmail.com>
2022-09-07 09:43:44 +1200

    Beginget method

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,22 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const config = require("./config.json");
 
-// -----Schema-----
-// const Project = require("./models/project.js");
+// -------------------------------------Schemas-----------------------------------
+const wildlife_post = require("./models/wildlife-post.js");
+const user = require("./models/user.js");
+const comments = require("./models/comments.js");
 
-// -----Start Dependencies-----
+// ----------------------------------Start Dependencies---------------------------
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// -----Start Server-----
+// -----------------------------------Start Server--------------------------------
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// -----Connect to MongoDB-----
+// ---------------------------------Connect to MongoDB----------------------------
 mongoose
   .connect(
     `mongodb+srv://${config.username}:${config.password}@sum3db.cx7l0zw.mongodb.net/?retryWrites=true&w=majority`
@@ -34,3 +36,12 @@ mongoose
   .catch((err) => {
     console.log(`DB connection error ${err.message}`);
   });
+
+// ==============================================================================
+//                                  GET METHOD
+// ==============================================================================
+app.get("/allWildlifePosts", (req, res) => {
+  WildlifePost.find().then((result) => {
+    res.send(result);
+  });
+});
