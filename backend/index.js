165babe3fa7deb7692e59032eeedc6aaa8c0bf75
hollyjohanna <hollyjohanna.robbins@gmail.com>
2022-09-12 10:58:03 +1200

    added login and register backend code, working on getting it working to pages

--- backend/index.js
+++ backend/index.js
@@ -45,3 +45,50 @@ app.get("/allWildlifePosts", (req, res) => {
     res.send(result);
   });
 });
+
+// ==============================================================================
+//                                 SIGN UP USERS
+// ==============================================================================
+app.post("/registerUser", (req, res) => {
+  // Checking if user is in the DB already
+
+  User.findOne({ username: req.body.username }, (err, userResult) => {
+    if (userResult) {
+      // send back a string so we can validate the user
+      res.send("username exists");
+    } else {
+      const hash = bcrypt.hashSync(req.body.password); // Encrypt User Password
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        password: hash,
+        profile_img_url: req.body.profile_img_url,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          // Save to database and notify userResult
+          res.send(result);
+        })
+        .catch((err) => res.send(err));
+    } // Else
+  });
+}); // End of Create Account
+
+// ==============================================================================
+//                                  LOGIN
+// ==============================================================================
+app.post("/loginUser", (req, res) => {
+  User.findOne({ username: req.body.username }, (err, userResult) => {
+    if (userResult) {
+      if (bcrypt.compareSync(req.body.password, userResult.password)) {
+        res.send(userResult);
+      } else {
+        res.send("not authorised");
+      } // inner if
+    } else {
+      res.send("user not found");
+    } // outer if
+  }); // Find one ends
+}); // end of post login
