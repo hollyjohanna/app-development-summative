be5fe43b3169ea9ffa8bd8ecab21c527954ff2ba
jaruuude <jaredrclarke@outlook.co.nz>
2022-09-13 08:56:47 +1200

    Additional masonry styling, created 'Create Post' button, more post styling.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,7 +3,7 @@ console.log("hello");
 // ===============================================================================
 //                               SHOW ALL POSTS & RENDER
 // ===============================================================================
-const result = document.getElementById("result");
+const grid = document.getElementById("feed-grid-cont");
 
 let showAllPosts = () => {
   $.ajax({
@@ -21,10 +21,26 @@ let showAllPosts = () => {
 
 let renderPosts = (wildlifePosts) => {
   console.log("The render post function is running");
-  result.innerHTML = "";
+  grid.innerHTML = "";
   wildlifePosts.forEach((item, index) => {
-    result.innerHTML += `
-          <img src="${item.image_url}" alt="${item.name}">
+    grid.innerHTML += `
+      <div class="grid-item post">
+        <div class="post-data-cont-top">
+          <div class="post-avatar-cont">
+            <img alt="">
+          </div>
+          <div class="post-like-cont">
+            <i class="bi bi-heart"></i>
+          </div>
+        </div>
+        <div class="post-data-cont-bottom">
+          <i class="bi bi-geo-fill"></i>
+          <h4 class="post-location">${item.location}</h4>
+          <p class="view-more">View more</p>
+        </div>
+        <div class="post-hover"></div>
+        <img src="${item.image_url}" alt="${item.name}" class="post-img">
+      </div>
         `;
   });
 };
