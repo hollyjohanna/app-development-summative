ee5d6f4f9c4d1acc1df9fcf956c47ad7ab2e26e4
jaruuude <jaredrclarke@outlook.co.nz>
2022-09-14 12:00:04 +1200

    Added Post css

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -17,17 +17,17 @@ const captionInput = document.getElementById("caption-input");
 const grid = document.getElementById("feed-grid-cont");
 
 let showAllPosts = () => {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/allWildlifePosts",
-        success: (posts) => {
-            console.log(posts);
-            renderPosts(posts);
-        },
-        error: (error) => {
-            console.log(error);
-        },
-    });
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/allWildlifePosts",
+    success: (posts) => {
+      console.log(posts);
+      renderPosts(posts);
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
 };
 
 let renderPosts = (wildlifePosts) => {
@@ -53,7 +53,7 @@ let renderPosts = (wildlifePosts) => {
         <img src="${item.image_url}" alt="${item.name}" class="post-img">
       </div>
         `;
-    });
+  });
 };
 
 showAllPosts();
@@ -63,26 +63,36 @@ showAllPosts();
 // ==========================================================
 
 addPostBtn.onclick = () => {
-    console.log("clicked");
-    $.ajax({
-        url: `http://localhost:3000/addWildlifePost`,
-        // use the post type to create data somewhere
-        // requesting to POST our data
-        type: "POST",
-        // we can send objects through to the backend, using the data argument
-        data: {
-            // the first property (i.e. the one on the left) called name has to be spelt exactly as the schema
-            image_url: imageURLInput.value,
-            title: titleInput.value,
-            location: locationInput.value,
-            caption: captionInput.value,
-        },
-        success: () => {
-            console.log("A new student was added.");
-            showAllPosts();
-        },
-        error: () => {
-            console.log("Error: cannot reach the backend");
-        },
-    });
+  console.log("clicked");
+  $.ajax({
+    url: `http://localhost:3000/addWildlifePost`,
+    // use the post type to create data somewhere
+    // requesting to POST our data
+    type: "POST",
+    // we can send objects through to the backend, using the data argument
+    data: {
+      // the first property (i.e. the one on the left) called name has to be spelt exactly as the schema
+      image_url: imageURLInput.value,
+      title: titleInput.value,
+      location: locationInput.value,
+      caption: captionInput.value,
+      author: {
+        author_name: sessionStorage.userName,
+        author_image_url: sessionStorage.profileImg,
+        author_id: sessionStorage.userID
+      }
+
+
+
+
+    },
+    success: () => {
+      console.log("A new student was added.");
+      showAllPosts();
+    },
+    error: () => {
+      console.log("Error: cannot reach the backend");
+    },
+  });
 };
+
